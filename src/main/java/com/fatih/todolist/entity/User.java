ed0ee55336faf9ec8dbcf020a09ef6cf1c7edb3c
fatihtavsan <fatihtavsan981@gmail.com>
2025-01-12 15:12:29 +0300

    feat: Create User entity

---
+++ src/main/java/com/fatih/todolist/entity/User.java
@@ -0,0 +1,32 @@
+package com.fatih.todolist.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_name")
+    private Long id;
+
+    @Column(name = "user_name", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "user_password", nullable = false)
+    private String password;
+
+    @Column(name = "user_role", nullable = false)
+    @Pattern(regexp = "^(ADMIN|USER)$", message = "Role must be either ADMIN or USER")
+    private String role;
+}
